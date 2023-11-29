8dbbf354a3684ceb5fbfe54a18396c5200375d7b
the_byte_bender <cccefg2@gmail.com>
2023-11-29 20:53:36 +0300

    refactor: Export extensions

--- lib/flutter_al.dart
+++ lib/flutter_al.dart
@@ -6,3 +6,4 @@ export "src/buffer.dart" show Buffer;
 export "src/source.dart" show Source;
 export "src/buffer_format.dart" show BufferFormat;
 export "src/source_state.dart" show SourceState;
+export "src/extensions.dart" show AtomicSourceOperations;
